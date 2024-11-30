c68ef21106f30c19a6e76274baf378e076b42791
Josiah Voight <jovoight@gmail.com>
2024-11-29 22:04:54 -0600

    Bishop class

--- src/main/java/jade/board/Board.java
+++ src/main/java/jade/board/Board.java
@@ -1,7 +1,10 @@
 package jade.board;
 
+import jade.Team;
+import jade.pieces.Bishop;
+
 public class Board {
     public static Square getSquare(final int index) {
-        return null;
+        return Square.createSquare(index, new Bishop(0, Team.WHITE));
     }
 }
---
+++ src/main/java/jade/pieces/Bishop.java
@@ -0,0 +1,57 @@
+package jade.pieces;
+
+import jade.Team;
+import jade.board.Board;
+import jade.board.BoardUtils;
+import jade.board.Move;
+import jade.board.Square;
+import static jade.board.Move.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Bishop extends Piece {
+
+    private final static int[] POSSIBLE_MOVES = { -9, -7, 7, 9 };
+
+    public Bishop(int position, Team team) {
+        super(position, team);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int move: POSSIBLE_MOVES) {
+            int destination = this.position;
+            while (BoardUtils.isValidDestination(destination)) {
+                // handle edge cases
+                if (
+                    isFirstColumnExclusion(destination, move)
+                    || isEighthColumnExclusion(destination, move)
+                ) { break; }
+                if (BoardUtils.isValidDestination(destination)) {
+                    destination += move;
+                    // check if occupied
+                    final Square destinationSquare = Board.getSquare(destination);
+                    if (!destinationSquare.isOccupied()) { legalMoves.add(new NormalMove(board, this, destination)); }
+                    else {
+                        final Piece occupier = destinationSquare.getPiece();
+                        if (this.team != occupier.getTeam()) { legalMoves.add(new Capture(board, this, destination, occupier)); }
+                        break;
+                    }
+                }
+            }
+        }
+        return legalMoves;
+    }
+
+    private static boolean isFirstColumnExclusion(final int position, final int move) {
+        return BoardUtils.FIRST_COLUMN[position] && (move == -9 || move == 7);
+    }
+    private static boolean isEighthColumnExclusion(final int position, final int move) {
+        return BoardUtils.EIGHTH_COLUMN[position] && (move == 9 || move == -7);
+    }
+}
